9c7d6380991cd4bb0c59843fca88c70cd1e46eac
ShangzeWu <56289293+ShangzeWu@users.noreply.github.com>
2021-12-22 16:42:04 +0800

    Update DminusABC.py

--- DminusABC.py
+++ DminusABC.py
@@ -95,13 +95,18 @@ wb3.close()
 
 #print(list_name)
 print(len(list_number))
+print(list_number)
 #删除D表中的进港件记录
 for x in range(2,Allrow4+1):
-    sendpoint = ws4.cell(x,11).value
-    sendpoint = str(sendpoint)
-    if sendpoint != '江苏省市场部五十七部' or sendpoint != '江苏盐城宝龙公司' or sendpoint != '江苏盐城公司' or sendpoint != '江苏盐城龙冈公司' or sendpoint != '江苏盐城亭湖公司' or sendpoint != '江苏盐城万达公司' or sendpoint != '江苏盐城吾悦公司' or sendpoint != '江苏盐城盐都公司' or sendpoint != '江苏盐城盐南高新公司' or sendpoint != '江苏盐城招商公司':
+    if ws4.cell(x,11).value == None:
         ws4.delete_rows(x)
         x-=1
+    else:
+        sendpoint = ws4.cell(x,11).value
+        sendpoint = str(sendpoint)
+        if sendpoint != '江苏省市场部五十七部' or sendpoint != '江苏盐城宝龙公司' or sendpoint != '江苏盐城公司' or sendpoint != '江苏盐城龙冈公司' or sendpoint != '江苏盐城亭湖公司' or sendpoint != '江苏盐城万达公司' or sendpoint != '江苏盐城吾悦公司' or sendpoint != '江苏盐城盐都公司' or sendpoint != '江苏盐城盐南高新公司' or sendpoint != '江苏盐城招商公司':
+            ws4.delete_rows(x)
+            x-=1
     
 Allrow4 = ws4.max_row
 
