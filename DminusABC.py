c40329268d9a4b511d9706113afac948824cbeaf
ShangzeWu <56289293+ShangzeWu@users.noreply.github.com>
2021-12-23 09:21:56 +0800

    Update DminusABC.py

--- DminusABC.py
+++ DminusABC.py
@@ -33,8 +33,6 @@ dir_B = path+'/uploadB/' #用来读取B文件 的 路径
 dir_C = path+'/uploadC/' #用来读取C文件 的 路径
 dir_D = path+'/uploadD/' #用来读取D文件 的 路径
 
-#dir_namelist = path+'/namelist/' #用来读取人员名单表 的 路径
-#dir_template = path+'/template/template.xlsx' #用来读取输出模版表格 的 路径
 dir_save_D= "/var/www/html/RecvSend/resultD/"  #输出 D文件 的保存路径
 
 file_name_A = find_new_file(dir_A)
@@ -42,23 +40,32 @@ file_name_B = find_new_file(dir_B)
 file_name_C = find_new_file(dir_C)
 file_name_D = find_new_file(dir_D)
 
-#file_name_list = find_new_file(dir_namelist)
-
 #业务逻辑
+wb4 = load_workbook(dir_D+file_name_D) #D表
+ws4 = wb4[wb4.sheetnames[0]]           #D表第一页
+
+#读取名单表，写入数组
+Allrow4 = ws4.max_row
+
+#删除D表中的进港件记录
+for x in range(2,Allrow4+1):
+    if ws4.cell(x,11).value == None:
+        setNull(ws4,x)  #清空整行内容
+    else:
+        sendpoint = ws4.cell(x,11).value
+        sendpoint = str(sendpoint)
+        if sendpoint != '江苏省市场部五十七部' and sendpoint != '江苏盐城公司' and sendpoint != '江苏盐城宝龙公司' and sendpoint != '江苏盐城龙冈公司' and sendpoint != '江苏盐城亭湖公司' and sendpoint != '江苏盐城万达公司' and sendpoint != '江苏盐城吾悦公司' and sendpoint != '江苏盐城盐都公司' and sendpoint != '江苏盐城盐南高新公司' and sendpoint != '江苏盐城招商公司':
+            setNull(ws4,x)  #清空整行内容
+
+
+#print(Allrow4)
 wb1 = load_workbook(dir_A+file_name_A) #A表
 ws1 = wb1[wb1.sheetnames[0]]           #A表第一页
 wb2 = load_workbook(dir_B+file_name_B) #B表
 ws2 = wb2[wb2.sheetnames[0]]           #B表第一页
 wb3 = load_workbook(dir_C+file_name_C) #C表
 ws3 = wb3[wb3.sheetnames[0]]           #C表第一页
-wb4 = load_workbook(dir_D+file_name_D) #D表
-ws4 = wb4[wb4.sheetnames[0]]           #D表第一页
 
-# wb2 = load_workbook(dir_namelist+file_name_list)      #namelist表
-# ws2 = wb2[wb2.sheetnames[0]]           #namelist表第一页
-# wb3 = load_workbook(dir_template)      #模板表
-
-#读取名单表，写入数组
 Allrow1 = ws1.max_row
 Allcol1 = ws1.max_column
 Allrow2 = ws2.max_row
@@ -66,8 +73,7 @@ Allcol2 = ws2.max_column
 Allrow3 = ws3.max_row
 Allcol3 = ws3.max_column
 Allrow4 = ws4.max_row
-#print(Allrow4)
-#Allcol2 = ws2.max_column
+
 list_number = ['9999999999999']
 
 for n in range(2, Allrow1+1):   #读取A表的单号序列,存入列表
@@ -98,32 +104,7 @@ for o in range(2, Allrow3+1):    #读取C表的单号序列
     else:
         value_number = str(value_number)
         list_number.append(value_number)
-wb3.close()         
-#wb3.save(dir_C+file_name_C)
-
-#print(list_name)
-#print(len(list_number))
-#print(list_number)
-#删除D表中的进港件记录
-for x in range(2,Allrow4+1):
-    if ws4.cell(x,11).value == None:
-        setNull(ws4,x)  #清空整行内容
-        #print(x)
-        #x-=1
-    else:
-        sendpoint = ws4.cell(x,11).value
-        sendpoint = str(sendpoint)
-        if sendpoint != '江苏省市场部五十七部' and sendpoint != '江苏盐城公司' and sendpoint != '江苏盐城宝龙公司' and sendpoint != '江苏盐城龙冈公司' and sendpoint != '江苏盐城亭湖公司' and sendpoint != '江苏盐城万达公司' and sendpoint != '江苏盐城吾悦公司' and sendpoint != '江苏盐城盐都公司' and sendpoint != '江苏盐城盐南高新公司' and sendpoint != '江苏盐城招商公司':
-            setNull(ws4,x)  #清空整行内容
-            #print(sendpoint)
-            #print(x)
-            #x-=1
-            #print(x)
-            #print(" *********** ")
-    
-Allrow4 = ws4.max_row
-#print(Allrow4)
-
+wb3.close() 
 #删除D表中ABC的重复项
 for y in range(2,Allrow4+1):
     if ws4.cell(y,1).value == None:
