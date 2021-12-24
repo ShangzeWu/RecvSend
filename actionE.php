7ade49c25ace15347a5cd80cf93fc1e7a3dd6516
ShangzeWu <56289293+ShangzeWu@users.noreply.github.com>
2021-12-24 16:12:50 +0800

    Create actionE.php

---
+++ actionE.php
@@ -0,0 +1,17 @@
+<?php
+
+header("content-type:text/html;charset=utf-8");
+
+exec("python3 /var/www/html/RecvSend/EminusF.py 2>&1",$out,$ret);
+print_r($out);
+print_r($ret);
+
+
+#$shell="python3 preForA.py";
+#$a=exec($shell."2>error.txt",$array,$ret);
+#print_r($a);
+#echo $return_var;
+
+
+header("Location: http://47.114.178.105/RecvSend/resultD");
+?>
