400421bc59fd6616e3495ccc0421f46dc2118365
ShangzeWu <56289293+ShangzeWu@users.noreply.github.com>
2021-12-25 17:43:43 +0800

    Update backupRec.sh

--- backupRec.sh
+++ backupRec.sh
@@ -7,3 +7,7 @@ find /var/www/html/RecvSend/uploadB/*  -name "*.xlsx*" -exec mv {} /var/www/html
 find /var/www/html/RecvSend/uploadC/*  -name "*.xlsx*" -exec mv {} /var/www/html/backupFiles/Recv/uploadC \;
 
 find /var/www/html/RecvSend/uploadD/*  -name "*.xlsx*" -exec mv {} /var/www/html/backupFiles/Recv/uploadD \;
+
+find /var/www/html/RecvSend/uploadE/*  -name "*.xlsx*" -exec mv {} /var/www/html/backupFiles/Recv/uploadE \;
+
+find /var/www/html/RecvSend/resultE/*  -name "*.xlsx*" -exec mv {} /var/www/html/backupFiles/Recv/resultE \;
